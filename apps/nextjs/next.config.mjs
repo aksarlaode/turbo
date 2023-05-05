72acc31ed29a69385a6afde6a5e2e98a02128423
aksarlaode <akhsaras21@gmail.com>
2023-05-05 01:49:10 +0000

    feat(create-turbo): install dependencies

---
+++ apps/nextjs/next.config.mjs
@@ -0,0 +1,21 @@
+import "./src/env.mjs";
+import withMdx from "@next/mdx";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@acme/api", "@acme/db"],
+  pageExtensions: ["ts", "tsx", "mdx"],
+
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+    typedRoutes: true,
+  },
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default withMdx()(config);
